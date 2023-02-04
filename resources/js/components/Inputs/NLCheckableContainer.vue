8ae322abe8ed712c9cd76df817c243c72c1f2487
Noual Ali <noualdev@gmail.com>
2023-02-04 22:08:46 +0100

    Correction des bugs pour les checkbox

--- resources/js/components/Inputs/NLCheckableContainer.vue
+++ resources/js/components/Inputs/NLCheckableContainer.vue
@@ -1,9 +1,12 @@
 <template>
-  <DefaultContainer :form="form" :name="name" :labelRequired="labelRequired" :label="label" :id="id">
-    <div class="form-check-container" :class="{ 'is-inline': isInline }">
-      <slot></slot>
-    </div>
-  </DefaultContainer>
+  <div class="form-check-container" :class="{ 'is-inline': isInline }">
+    <label class="form-label" :for="id" :class="{ 'text-danger': form?.errors.has(name), 'is-required': labelRequired }"
+      v-if="label">
+      {{ $t(label) }}
+    </label>
+    <slot></slot>
+    <has-error :form="form" :field="name" class="text-danger" v-if="form" />
+  </div>
 </template>
 
 <script>
@@ -15,10 +18,10 @@ export default {
   props: {
     form: { type: Object, required: false },
     name: { type: String, required: true },
+    id: { type: String, required: true },
     label: { type: String, default: '' },
     labelRequired: { type: Boolean, default: false },
     isInline: { type: Boolean, default: true },
-    id: { type: String, default: null },
   },
   computed: {
     getId() {
--- resources/js/components/Inputs/NLCheckbox.vue
+++ resources/js/components/Inputs/NLCheckbox.vue
@@ -1,39 +1,47 @@
 <template>
-  <div class="form-check">
-    <label :for="id">
-      <input v-on="$listeners" class="input-checkable" type="checkbox" :value="value" :name="name" :id="id"
-        @input="$emit('update', $event.target.value)">
-      <div class="checkable is-checkbox"></div>
-      {{ label }}
-    </label>
-  </div>
+  <NLCheckableContainer :id="getId" :form="form" :name="name" :labelRequired="labelRequired">
+    <div class="form-check">
+      <label :for="getId">
+        <input type="checkbox" :class="[{ 'is-danger': form?.errors.has(name) }, 'input-checkable']" :id="getId"
+          :value="selected" @input="updateValue" :name="name">
+        <div class="checkable is-checkbox"></div>
+        {{ label }}
+      </label>
+    </div>
+  </NLCheckableContainer>
 </template>
 
 <script>
-
+import NLCheckableContainer from './NLCheckableContainer'
 export default {
+  components: { NLCheckableContainer },
   name: "NLCheckbox",
   props: {
     form: { type: Object, required: false },
     name: { type: String, required: true },
-    label: { type: String, required: true },
-    value: { type: String | Number | Boolean, default: undefined },
+    id: { type: String, default: null },
+    label: { type: String, default: '' },
+    labelRequired: { type: Boolean, default: false },
+    value: { type: String | Number | Boolean, default: false },
   },
   model: {
-    prop: "checkedValue",
-    event: "update"
+    prop: 'value',
+    event: 'change'
+  },
+  data() {
+    return {
+      selected: this.value,
+    };
   },
   computed: {
-    updateCheckedVal() {
-      const index = this.form[ this.name ].indexOf(this.value)
-      if (index > -1) {
-        this.form[ this.name ].splice(index, 1)
-      } else {
-        this.form[ this.name ].push(this.value)
-      }
-    },
-    id() {
-      return `${this.name}-${this.value}`
+    getId() {
+      return this.id ? this.id : this.name
+    }
+  },
+  methods: {
+    updateValue(event) {
+      this.selected = event.target.checked
+      this.$emit('change', this.selected)
     }
   }
 }
