4481c7e7a55fc7df946e2de76d12206c577c9e72
Noual Ali <noualdev@gmail.com>
2023-02-06 15:42:04 +0100

    Finalisation du centre de notification

--- app/Http/Resources/NotificationResource.php
+++ app/Http/Resources/NotificationResource.php
@@ -17,12 +17,20 @@ class NotificationResource extends JsonResource
     {
         $createdAt = $this->created_at ? Carbon::parse($this->created_at)->diffForHumans() : '-';
         $readAt = $this->read_at ? Carbon::parse($this->read_at)->diffForHumans() : '-';
+        $content = isset($this->data['content']) ? $this->data['content'] : '-';
+        $title = isset($this->data['title']) ? $this->data['title'] : '-';
+        $routeName = isset($this->data['routeName']) ? $this->data['routeName'] : '-';
+        $paramNames = isset($this->data['paramNames']) ? $this->data['paramNames'] : '-';
+        $modelId = isset($this->data['id']) ? $this->data['id'] : '-';
         return [
             'id' => $this->id,
-            'content' => $this->data['content'],
-            'title' => $this->data['title'],
+            'content' => $content,
+            'title' => $title,
             'created_at' => $createdAt,
             'read_at' => $readAt,
+            'routeName' => $routeName,
+            'paramNames' => $paramNames,
+            'modelId' => $modelId,
         ];
     }
 }
