1e8b6c04829d89d8ef574b5d4a1d3e858834390a
Noual Ali <noualdev@gmail.com>
2023-02-06 11:06:47 +0100

    Correction de v-can && ajout de la directive v-has-role

--- resources/js/plugins/acl.js
+++ resources/js/plugins/acl.js
@@ -1,3 +1,4 @@
+
 import Vue from "vue";
 import api from "./api";
 import { user } from "./helpers";
@@ -11,9 +12,15 @@ api.get('user').then(success => {
  */
 Vue.directive('can', {
   bind: function (el, binding, vnode) {
+    const hasIfCond = vnode.data.directives.find(directive => directive.name === 'if')
+    const ifCondResult = hasIfCond ? vnode.context[ vifDirective.expression ] : true
+    const hasElseCond = vnode.data.directives.find(directive => directive.name === 'else')
+    const elseCondResult = hasElseCond ? false : true
     const authorizations = JSON.parse(localStorage.getItem('authorizations'))
     const abilities = binding.value.split(',') || binding.value.split(', ')
     let hasAbilities = []
+    hasAbilities.push(ifCondResult)
+    hasAbilities.push(elseCondResult)
     abilities.forEach((ability) => {
       ability = ability.trim()
       hasAbilities.push(!authorizations[ ability ] || authorizations[ ability ] == undefined)
@@ -40,10 +47,41 @@ Vue.directive('can', {
     }
   }
 })
-// Vue.directive('cannot', {
-//   bind: function (el, binding, vnode) {
-//     const authorizations = JSON.parse(localStorage.getItem('authorizations'))
-//     return !(!authorizations[ binding.value ] || authorizations[ binding.value ] == undefined)
-//   }
-// })
+
+/**
+ * v-has-role: Check user roles to determine if he can perform or not some action
+ */
+Vue.directive('has-role', {
+  bind: function (el, binding, vnode) {
+    const roles = JSON.parse(localStorage.getItem('roles'))
+    const data = binding.value.split(',') || binding.value.split(', ')
+    let hasRoles = []
+    data.forEach((item) => {
+      item = item.trim()
+      roles.forEach(role => {
+        hasRoles.push(role.code == item)
+      })
+    })
+    if (!hasRoles.includes(true)) {
+      const comment = document.createComment(' ');
+      Object.defineProperty(comment, 'setAttribute', {
+        value: () => undefined,
+      });
+      vnode.elm = comment;
+      vnode.text = ' ';
+      vnode.isComment = true;
+      vnode.context = undefined;
+      vnode.tag = undefined;
+      vnode.data.directives = undefined;
+
+      if (vnode.componentInstance) {
+        vnode.componentInstance.$el = comment;
+      }
+
+      if (el.parentNode) {
+        el.parentNode.replaceChild(comment, el);
+      }
+    }
+  }
+})
 
