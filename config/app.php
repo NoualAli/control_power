d95fee9161b6f62628b21db4d38a93fb73761299
Ali Noual <noualdev@gmail.com>
2023-02-11 14:40:27 +0100

    Ajout de Image intervention

--- config/app.php
+++ config/app.php
@@ -195,7 +195,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Intervention\Image\ImageServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -256,7 +256,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Image' => Intervention\Image\Facades\Image::class,
     ],
 
 ];
