2f717bf5f6f18d5515cd80c1e9317da148c6e677
Ali Noual <noualdev@gmail.com>
2023-02-12 22:14:41 +0100

    Refactorisation de v-can directive

--- resources/js/plugins/acl.js
+++ resources/js/plugins/acl.js
@@ -12,39 +12,35 @@ api.get('user').then(success => {
  */
 Vue.directive('can', {
   bind: function (el, binding, vnode) {
-    const hasIfCond = vnode.data.directives.find(directive => directive.name === 'if')
-    const ifCondResult = hasIfCond ? vnode.context[ vifDirective.expression ] : true
-    const hasElseCond = vnode.data.directives.find(directive => directive.name === 'else')
-    const elseCondResult = hasElseCond ? false : true
-    const authorizations = JSON.parse(localStorage.getItem('authorizations'))
-    const abilities = binding.value.split(',') || binding.value.split(', ')
-    let hasAbilities = []
-    hasAbilities.push(ifCondResult)
-    hasAbilities.push(elseCondResult)
-    abilities.forEach((ability) => {
-      ability = ability.trim()
-      hasAbilities.push(!authorizations[ ability ] || authorizations[ ability ] == undefined)
-    })
-    if (!hasAbilities.includes(false)) {
-      const comment = document.createComment(' ');
-      Object.defineProperty(comment, 'setAttribute', {
-        value: () => undefined,
+    const vifDirective = vnode.data.directives.find(directive => directive.name === 'if');
+    const authorizations = JSON.parse(localStorage.getItem('authorizations')) || [];
+    const abilities = (binding.value || '').split(/\s*,\s*/);
+    if (abilities.every(ability => !authorizations.hasOwnProperty(ability.trim()) && !authorizations[ ability.trim() ]) && (!vifDirective || vnode.context[ vifDirective.expression ])) {
+      abilities.forEach(ability => {
+        if (!authorizations.hasOwnProperty(ability.trim())) {
+          const comment = document.createComment(' ');
+          Object.defineProperty(comment, 'setAttribute', {
+            value: () => undefined
+          });
+          vnode.elm = comment;
+          vnode.text = ' ';
+          vnode.isComment = true;
+          vnode.context = undefined;
+          vnode.tag = undefined;
+          vnode.data.directives = undefined;
+
+          if (vnode.componentInstance) {
+            vnode.componentInstance.$parent = comment
+            vnode.componentInstance.$el = comment;
+          }
+
+          if (el.parentNode) {
+            el.parentNode.replaceChild(comment, el);
+          }
+        }
       });
-      vnode.elm = comment;
-      vnode.text = ' ';
-      vnode.isComment = true;
-      vnode.context = undefined;
-      vnode.tag = undefined;
-      vnode.data.directives = undefined;
-
-      if (vnode.componentInstance) {
-        vnode.componentInstance.$el = comment;
-      }
-
-      if (el.parentNode) {
-        el.parentNode.replaceChild(comment, el);
-      }
     }
+
   }
 })
 
@@ -53,20 +49,16 @@ Vue.directive('can', {
  */
 Vue.directive('has-role', {
   bind: function (el, binding, vnode) {
-    const roles = JSON.parse(localStorage.getItem('roles'))
-    const data = binding.value.split(',') || binding.value.split(', ')
-    let hasRoles = []
-    data.forEach((item) => {
-      item = item.trim()
-      roles.forEach(role => {
-        hasRoles.push(role.code == item)
-      })
-    })
-    if (!hasRoles.includes(true)) {
+    const roles = JSON.parse(localStorage.getItem('roles')) || [];
+    const data = (binding.value || '').split(/\s*,\s*/);
+    const hasRole = data.some(item => roles.some(role => role.code === item.trim()));
+
+    if (!hasRole) {
       const comment = document.createComment(' ');
       Object.defineProperty(comment, 'setAttribute', {
-        value: () => undefined,
+        value: () => undefined
       });
+
       vnode.elm = comment;
       vnode.text = ' ';
       vnode.isComment = true;
@@ -82,6 +74,7 @@ Vue.directive('has-role', {
         el.parentNode.replaceChild(comment, el);
       }
     }
+
   }
 })
 
