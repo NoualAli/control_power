adac50afd831826fd4d76ac2637a5ab0e2e81cb9
Noual Ali <noualdev@gmail.com>
2023-02-13 14:01:32 +0100

    Refactorisation et correction du bug pour la directive v-can

--- resources/js/plugins/acl.js
+++ resources/js/plugins/acl.js
@@ -1,46 +1,25 @@
 
 import Vue from "vue";
 import api from "./api";
-import { user } from "./helpers";
 
 api.get('user').then(success => {
   localStorage.setItem("authorizations", JSON.stringify(success.data.authorizations));
 });
-
 /**
  * v-can: Check user abilities to determine if he can perform or not some action
- */
+*/
 Vue.directive('can', {
   bind: function (el, binding, vnode) {
-    const vifDirective = vnode.data.directives.find(directive => directive.name === 'if');
+    let can = []
     const authorizations = JSON.parse(localStorage.getItem('authorizations')) || [];
     const abilities = (binding.value || '').split(/\s*,\s*/);
-    if (abilities.every(ability => !authorizations.hasOwnProperty(ability.trim()) && !authorizations[ ability.trim() ]) && (!vifDirective || vnode.context[ vifDirective.expression ])) {
-      abilities.forEach(ability => {
-        if (!authorizations.hasOwnProperty(ability.trim())) {
-          const comment = document.createComment(' ');
-          Object.defineProperty(comment, 'setAttribute', {
-            value: () => undefined
-          });
-          vnode.elm = comment;
-          vnode.text = ' ';
-          vnode.isComment = true;
-          vnode.context = undefined;
-          vnode.tag = undefined;
-          vnode.data.directives = undefined;
-
-          if (vnode.componentInstance) {
-            vnode.componentInstance.$parent = comment
-            vnode.componentInstance.$el = comment;
-          }
-
-          if (el.parentNode) {
-            el.parentNode.replaceChild(comment, el);
-          }
-        }
-      });
+    abilities.some(ability => {
+      can.push(!authorizations.hasOwnProperty(ability.trim()))
+    });
+    if (can.every(value => value === true)) {
+      customComment(vnode, el)
     }
-
+    return
   }
 })
 
@@ -52,29 +31,40 @@ Vue.directive('has-role', {
     const roles = JSON.parse(localStorage.getItem('roles')) || [];
     const data = (binding.value || '').split(/\s*,\s*/);
     const hasRole = data.some(item => roles.some(role => role.code === item.trim()));
-
     if (!hasRole) {
-      const comment = document.createComment(' ');
-      Object.defineProperty(comment, 'setAttribute', {
-        value: () => undefined
-      });
-
-      vnode.elm = comment;
-      vnode.text = ' ';
-      vnode.isComment = true;
-      vnode.context = undefined;
-      vnode.tag = undefined;
-      vnode.data.directives = undefined;
-
-      if (vnode.componentInstance) {
-        vnode.componentInstance.$el = comment;
-      }
-
-      if (el.parentNode) {
-        el.parentNode.replaceChild(comment, el);
-      }
+      customComment(vnode, el)
     }
-
+    return
   }
 })
 
+/**
+ * Create a custom comment
+ *
+ * @param {Object} vnode
+ * @param {Object} el
+ *
+ * @return {void}
+ */
+function customComment(vnode, el) {
+  const comment = document.createComment(' ');
+  Object.defineProperty(comment, 'setAttribute', {
+    value: () => undefined
+  });
+  vnode.elm = comment;
+  vnode.text = ' ';
+  vnode.isComment = true;
+  vnode.context = undefined;
+  vnode.tag = undefined;
+  vnode.data.directives = undefined;
+
+  if (vnode.componentInstance) {
+    vnode.componentInstance.$el.classList.add('d-none')
+    vnode.componentInstance.$el.style = 'display:none !important'
+    vnode.componentInstance.$el = comment;
+  }
+
+  if (el.parentNode) {
+    el.parentNode.replaceChild(comment, el);
+  }
+}
