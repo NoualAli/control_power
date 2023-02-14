985408f530d906aa3d58097fdb1add0ad3423312
Noual Ali <noualdev@gmail.com>
2023-02-14 11:49:03 +0100

    Suppression du middleware locale

--- resources/js/plugins/i18n.js
+++ resources/js/plugins/i18n.js
@@ -14,6 +14,9 @@ const i18n = new VueI18n({
  * @param {String} locale
  */
 export async function loadMessages(locale) {
+  if (!locale) {
+    locale = 'fr'
+  }
   if (Object.keys(i18n.getLocaleMessage(locale)).length === 0) {
     const messages = await import(/* webpackChunkName: '' */ `~/lang/${locale}`)
     i18n.setLocaleMessage(locale, messages)
@@ -23,9 +26,5 @@ export async function loadMessages(locale) {
     i18n.locale = locale
   }
 }
-
-; (async function () {
-  await loadMessages(store.getters[ 'lang/locale' ])
-})()
 
 export default i18n
