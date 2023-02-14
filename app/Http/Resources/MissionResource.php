db6a8e8e4797f04aaf18d48adb51f4039651f53d
Noual Ali <noualdev@gmail.com>
2023-02-14 20:57:39 +0100

    Changement de la colonne controllers_str à agency_controllers_str

--- app/Http/Resources/MissionResource.php
+++ app/Http/Resources/MissionResource.php
@@ -25,7 +25,7 @@ class MissionResource extends JsonResource
             'state' => $this->realisation_state,
             'end' => $this->end,
             'start' => $this->start,
-            'controllers_str' => $this->controllers_str,
+            'agency_controllers_str' => $this->agency_controllers_str,
             'remaining_days_before_start' => $this->remaining_days_before_start,
             'progress_status' => $this->progress_status,
             'avg_score' => $this->avg_score
