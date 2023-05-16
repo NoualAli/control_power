aa3e3f908115f0a1c04887f3a0f48dba32d72845
nldev <noualdev@gmail.com>
2023-05-16 18:50:59 +0100

    Correction des labels

--- resources/js/pages/admin/users/create.vue
+++ resources/js/pages/admin/users/create.vue
@@ -1,120 +1,123 @@
 <template>
-  <div v-if="can('create_user')">
-    <ContentHeader title="Ajouter un utilisateur" />
-    <ContentBody>
-      <form @submit.prevent="create" @keydown="form.onKeydown($event)">
-        <div class="grid gap-10 my-4">
-          <!-- Firstname -->
-          <div class="col-12 col-lg-6 col-md-6">
-            <NLInput v-model="form.first_name" :form="form" name="firstname" label="firstname" />
-          </div>
+    <div v-if="can('create_user')">
+        <ContentHeader title="Ajouter un utilisateur" />
+        <ContentBody>
+            <form @submit.prevent="create" @keydown="form.onKeydown($event)">
+                <div class="grid gap-10 my-4">
+                    <!-- Firstname -->
+                    <div class="col-12 col-lg-6 col-md-6">
+                        <NLInput v-model="form.first_name" :form="form" name="firstname" label="Prénom" />
+                    </div>
 
-          <!-- Lastname -->
-          <div class="col-12 col-lg-6 col-md-6">
-            <NLInput v-model="form.last_name" :form="form" name="last_name" label="lastname" />
-          </div>
+                    <!-- Lastname -->
+                    <div class="col-12 col-lg-6 col-md-6">
+                        <NLInput v-model="form.last_name" :form="form" name="last_name" label="Nom de famille" />
+                    </div>
 
-          <!-- Username -->
-          <div class="col-12 col-lg-6 col-md-6">
-            <NLInput v-model="form.username" :form="form" name="username" label="username" label-required />
-          </div>
+                    <!-- Username -->
+                    <div class="col-12 col-lg-6 col-md-6">
+                        <NLInput v-model="form.username" :form="form" name="username" label="Nom d'utilisateur"
+                            label-required />
+                    </div>
 
-          <!-- Email -->
-          <div class="col-12 col-lg-6 col-md-6">
-            <NLInput v-model="form.email" :form="form" name="email" label="email" label-required type="email" />
-          </div>
+                    <!-- Email -->
+                    <div class="col-12 col-lg-6 col-md-6">
+                        <NLInput v-model="form.email" :form="form" name="email" label="Adresse e-mail" label-required
+                            type="email" />
+                    </div>
 
-          <!-- Phone -->
-          <div class="col-12 col-lg-6 col-md-6">
-            <NLInput v-model="form.phone" :form="form" name="phone" label="phone" type="phone" />
-          </div>
+                    <!-- Phone -->
+                    <div class="col-12 col-lg-6 col-md-6">
+                        <NLInput v-model="form.phone" :form="form" name="phone" label="N° de téléphone" type="phone" />
+                    </div>
 
-          <!-- Dres -->
-          <div class="col-12 col-lg-6 col-md-6">
-            <NLSelect v-model="form.dres" :form="form" name="dres" label="Dre" :options="dresList" :multiple="true" />
-          </div>
+                    <!-- Dres -->
+                    <div class="col-12 col-lg-6 col-md-6">
+                        <NLSelect v-model="form.dres" :form="form" name="dres" label="DRE" :options="dresList"
+                            placeholder="Choisissez une DRE" :multiple="true" />
+                    </div>
 
-          <!-- Roles -->
-          <div class="col-12">
-            <NLSelect v-model="form.roles" :form="form" name="roles" label="Rôles" :options="rolesList"
-              :multiple="true" />
-          </div>
+                    <!-- Roles -->
+                    <div class="col-12">
+                        <NLSelect v-model="form.roles" :form="form" name="roles" label="Rôles"
+                            placeholder="Choisissez un rôle" :options="rolesList" :multiple="true" />
+                    </div>
 
-          <!-- Password -->
-          <div class="col-12 col-lg-4">
-            <NLInput v-model="form.password" :form="form" label="Password" name="password" type="password"
-              label-required />
-          </div>
-          <!-- Password Confirmation -->
-          <div class="col-12 col-lg-4">
-            <NLInput v-model="form.password_confirmation" :form="form" label="confirm_password"
-              name="password_confirmation" type="password" label-required />
-          </div>
-        </div>
-        <!-- Submit Button -->
-        <div class="d-flex justify-end align-center">
-          <NLButton :loading="form.busy" label="Add" class="is-radius" />
-        </div>
-      </form>
-    </ContentBody>
-  </div>
+                    <!-- Password -->
+                    <div class="col-12 col-lg-4">
+                        <NLInput v-model="form.password" :form="form" label="Mot de passe" name="password" type="password"
+                            label-required />
+                    </div>
+                    <!-- Password Confirmation -->
+                    <div class="col-12 col-lg-4">
+                        <NLInput v-model="form.password_confirmation" :form="form" label="Confirmation du mot de passe"
+                            name="password_confirmation" type="password" label-required />
+                    </div>
+                </div>
+                <!-- Submit Button -->
+                <div class="d-flex justify-end align-center">
+                    <NLButton :loading="form.busy" label="Add" class="is-radius" />
+                </div>
+            </form>
+        </ContentBody>
+    </div>
 </template>
 
 <script>
 import { Form } from 'vform'
 import { mapGetters } from 'vuex'
 export default {
-  layout: 'MainLayout',
-  middleware: [ 'auth', 'admin' ],
-  data() {
-    return {
-      form: new Form({
-        username: null,
-        email: null,
-        first_name: null,
-        last_name: null,
-        gender: null,
-        password: null,
-        password_confirmation: null,
-        roles: [],
-        dres: []
-      }),
-      dresList: [],
-      rolesList: []
-    }
-  },
-  computed: mapGetters({
-    roles: 'roles/all',
-    dres: 'dre/all'
-  }),
-  created() {
-    this.$store.dispatch('roles/fetchAll').then(() => {
-      this.rolesList = this.roles.all
-    })
-    this.$store.dispatch('dre/fetchAll', { withAgencies: true }).then(() => {
-      this.dresList = this.dres.all
-    })
-  },
-  methods: {
-    create() {
-      this.form.post('/api/users').then(response => {
-        if (response.data.status) {
-          this.$swal.toast_success(response.data.message)
-          this.form.reset()
-        } else {
-          this.$swal.alert_error(response.data.message)
+    layout: 'MainLayout',
+    middleware: [ 'auth', 'admin' ],
+    data() {
+        return {
+            form: new Form({
+                username: null,
+                email: null,
+                first_name: null,
+                last_name: null,
+                gender: null,
+                password: null,
+                password_confirmation: null,
+                roles: [],
+                dres: []
+            }),
+            dresList: [],
+            rolesList: []
         }
-      }).catch(error => {
-        console.log(error)
-      })
     },
-    getPlaceholder(id) {
-      if (id) {
-        let placeholder = null
-        placeholder = this.dres?.all.filter((dre) => dre.id == id)
-        return placeholder[ 0 ] !== undefined ? placeholder[ 0 ]?.name : null
-      }
+    computed: mapGetters({
+        roles: 'roles/all',
+        dres: 'dre/all'
+    }),
+    created() {
+        this.$store.dispatch('roles/fetchAll').then(() => {
+            this.rolesList = this.roles.all
+        })
+        this.$store.dispatch('dre/fetchAll', { withAgencies: true }).then(() => {
+            this.dresList = this.dres.all
+        })
+    },
+    methods: {
+        create() {
+            this.form.post('/api/users').then(response => {
+                if (response.data.status) {
+                    this.$swal.toast_success(response.data.message)
+                    this.form.reset()
+                } else {
+                    this.$swal.alert_error(response.data.message)
+                }
+            }).catch(error => {
+                console.log(error)
+            })
+        },
+        getPlaceholder(id) {
+            if (id) {
+                let placeholder = null
+                placeholder = this.dres?.all.filter((dre) => dre.id == id)
+                return placeholder[ 0 ] !== undefined ? placeholder[ 0 ]?.name : null
+            }
+        }
     }
-  }
 }
 </script>
--- resources/js/pages/admin/users/edit.vue
+++ resources/js/pages/admin/users/edit.vue
@@ -1,173 +1,166 @@
 <template>
-  <div v-if="can('edit_user')">
-    <ContentBody>
-      <div class="grid gap-10 my-4">
-        <!-- Update User informations -->
-        <div class="col-12">
-          <form @submit.prevent="updateInfos" @keydown="form.onKeydown($event)">
+    <div v-if="can('edit_user')">
+        <ContentBody>
             <div class="grid gap-10 my-4">
-              <!-- Firstname -->
-              <div class="col-12 col-lg-6 col-md-6">
-                <NLInput v-model="form.first_name" :form="form" name="firstname" label="Prénom" label-required />
-              </div>
+                <!-- Update User informations -->
+                <div class="col-12">
+                    <form @submit.prevent="updateInfos" @keydown="form.onKeydown($event)">
+                        <div class="grid gap-10 my-4">
+                            <!-- Firstname -->
+                            <div class="col-12 col-lg-6 col-md-6">
+                                <NLInput v-model="form.first_name" :form="form" name="firstname" label="Prénom"
+                                    label-required />
+                            </div>
 
-              <!-- Lastname -->
-              <div class="col-12 col-lg-6 col-md-6">
-                <NLInput v-model="form.last_name" :form="form" name="last_name" label="Nom de famille" label-required />
-              </div>
+                            <!-- Lastname -->
+                            <div class="col-12 col-lg-6 col-md-6">
+                                <NLInput v-model="form.last_name" :form="form" name="last_name" label="Nom de famille"
+                                    label-required />
+                            </div>
 
-              <!-- Username -->
-              <div class="col-12 col-lg-6 col-md-6">
-                <NLInput v-model="form.username" :form="form" name="username" label="Nom d'utilisateur" label-required />
-              </div>
+                            <!-- Username -->
+                            <div class="col-12 col-lg-6 col-md-6">
+                                <NLInput v-model="form.username" :form="form" name="username" label="Nom d'utilisateur"
+                                    label-required />
+                            </div>
 
-              <!-- Phone -->
-              <div class="col-12 col-lg-6 col-md-6">
-                <NLInput v-model="form.phone" :form="form" name="phone" label="N° de téléphone" type="phone" />
-              </div>
+                            <!-- Phone -->
+                            <div class="col-12 col-lg-6 col-md-6">
+                                <NLInput v-model="form.phone" :form="form" name="phone" label="N° de téléphone"
+                                    type="phone" />
+                            </div>
 
-              <!-- Email -->
-              <div class="col-12 col-lg-6 col-md-6">
-                <NLInput v-model="form.email" :form="form" name="email" label="Adresse e-mail" type="email"
-                  label-required />
-              </div>
+                            <!-- Email -->
+                            <div class="col-12 col-lg-6 col-md-6">
+                                <NLInput v-model="form.email" :form="form" name="email" label="Adresse e-mail" type="email"
+                                    label-required />
+                            </div>
 
-              <!-- Dres -->
-              <div class="col-12 col-lg-6 col-md-6">
-                <NLSelect v-model="form.dres" :form="form" name="dres" label="Dre" :options="dresList" :multiple="true" />
-              </div>
+                            <!-- Dres -->
+                            <div class="col-12 col-lg-6 col-md-6">
+                                <NLSelect v-model="form.dres" :form="form" name="dres" label="DRE"
+                                    placeholder="Choisissez une DRE" :options="dresList" :multiple="true" />
+                            </div>
 
-              <!-- Role -->
-              <div class="col-12">
-                <NLSelect v-model="form.roles" :form="form" name="roles" label="Rôles" :options="rolesList"
-                  :multiple="true" />
-              </div>
+                            <!-- Role -->
+                            <div class="col-12">
+                                <NLSelect v-model="form.roles" :form="form" name="roles" placeholder="Choisissez un rôle"
+                                    label="Rôles" :options="rolesList" :multiple="true" />
+                            </div>
+                        </div>
+
+                        <!-- Submit Button -->
+                        <div class="d-flex justify-end align-center">
+                            <NLButton :loading="form.busy" label="Update" class="is-radius" />
+                        </div>
+                    </form>
+                </div>
+
+                <!-- Update Password -->
+                <div class="col-12">
+                    <form @submit.prevent="updatePassword" @keydown="passwordForm.onKeydown($event)">
+                        <div class="grid gap-10 my-4">
+                            <!-- Password -->
+                            <div class="col-12 col-lg-4">
+                                <NLInput v-model="passwordForm.password" :form="passwordForm" label="Mot de passe"
+                                    name="password" type="password" label-required />
+                            </div>
+                            <!-- Password Confirmation -->
+                            <div class="col-12 col-lg-4">
+                                <NLInput v-model="passwordForm.password_confirmation" :form="passwordForm"
+                                    label="Confirmation mot de passe" name="password_confirmation" type="password"
+                                    label-required />
+                            </div>
+                        </div>
+                        <!-- Submit Button -->
+                        <div class="d-flex justify-end align-center">
+                            <NLButton :loading="passwordForm.busy" label="Update" class="is-radius" />
+                        </div>
+                    </form>
+                </div>
             </div>
-
-            <!-- Submit Button -->
-            <div class="d-flex justify-end align-center">
-              <NLButton :loading="form.busy" label="Update" class="is-radius" />
-            </div>
-          </form>
-        </div>
-
-        <!-- Update Password -->
-        <div class="col-12">
-          <form @submit.prevent="updatePassword" @keydown="passwordForm.onKeydown($event)">
-            <div class="grid gap-10 my-4">
-              <!-- Password -->
-              <div class="col-12 col-lg-4">
-                <NLInput v-model="passwordForm.password" :form="passwordForm" label="Mot de passe" name="password"
-                  type="password" label-required />
-              </div>
-              <!-- Password Confirmation -->
-              <div class="col-12 col-lg-4">
-                <NLInput v-model="passwordForm.password_confirmation" :form="passwordForm"
-                  label="Confirmation mot de passe" name="password_confirmation" type="password" label-required />
-              </div>
-            </div>
-            <!-- Submit Button -->
-            <div class="d-flex justify-end align-center">
-              <NLButton :loading="passwordForm.busy" label="Update" class="is-radius" />
-            </div>
-          </form>
-        </div>
-      </div>
-    </ContentBody>
-  </div>
+        </ContentBody>
+    </div>
 </template>
 
 <script>
-import NLSelect from '../../../components/Inputs/NLSelect'
-// import NLCheckableContainer from '../../../components/Inputs/NLCheckableContainer'
-// import NLRadio from '../../../components/Inputs/NLRadio'
-// import DefaultContainer from '../../../components/Inputs/DefaultContainer'
 import { Form } from 'vform'
 // import Notification from '../../../components/Notification.vue'
 import { mapGetters } from 'vuex'
-// eslint-disable-next-line camelcase
 export default {
-  components: {
-    NLSelect
-    // NLCheckableContainer,
-    // NLRadio,
-    // Notification,
-    // DefaultContainer
-  },
-  layout: 'MainLayout',
-  middleware: [ 'auth', 'admin' ],
-  data() {
-    return {
-      form: new Form({
-        username: null,
-        email: null,
-        first_name: null,
-        last_name: null,
-        phone: null,
-        roles: [],
-        dres: []
-      }),
-      passwordForm: new Form({
-        password: null,
-        password_confirmation: null
-      }),
-      dresList: [],
-      rolesList: []
-    }
-  },
-  computed: mapGetters({
-    user: 'users/current',
-    dres: 'dre/all',
-    roles: 'roles/all'
-  }),
-  created() {
-    this.$store.dispatch('roles/fetchAll').then(() => {
-      this.rolesList = this.roles.all
-    })
-    this.$store.dispatch('dre/fetchAll', { withAgencies: true }).then(() => {
-      this.dresList = this.dres.all
-    })
-    this.$store.dispatch('users/fetch', this.$route.params.user).then((result) => {
-      this.form.fill(this.user.current)
-      this.form.roles = this.user.current.roles.map(item => item.id)
-      console.log(this.user.current)
-      this.form.dres = this.user.current.agencies.map(item => item.id)
-    })
-  },
-  methods: {
-    updateInfos() {
-      this.$swal.confirm_update().then((action) => {
-        if (action.isConfirmed) {
-          this.form.put('/api/users/info/' + this.user.current.id).then(response => {
-            if (response.data.status) {
-              this.$swal.toast_success(response.data.message)
-              this.$router.push({ name: 'users-index' })
-            } else {
-              this.$swal.alert_error(response.data.message)
-            }
-          }).catch(error => {
-            console.log(error)
-          })
+    layout: 'MainLayout',
+    middleware: [ 'auth', 'admin' ],
+    data() {
+        return {
+            form: new Form({
+                username: null,
+                email: null,
+                first_name: null,
+                last_name: null,
+                phone: null,
+                roles: [],
+                dres: []
+            }),
+            passwordForm: new Form({
+                password: null,
+                password_confirmation: null
+            }),
+            dresList: [],
+            rolesList: []
         }
-      })
     },
-    updatePassword() {
-      this.$swal.confirm_update().then((action) => {
-        if (action.isConfirmed) {
-          this.passwordForm.put('/api/users/password/' + this.user.current.id).then(response => {
-            if (response.data.status) {
-              this.$swal.toast_success(response.data.message)
-              this.passwordForm.reset()
-              this.$router.push({ name: 'users-index' })
-            } else {
-              this.$swal.alert_error(response.data.message)
-            }
-          }).catch(error => {
-            console.log(error)
-          })
+    computed: mapGetters({
+        user: 'users/current',
+        dres: 'dre/all',
+        roles: 'roles/all'
+    }),
+    created() {
+        this.$store.dispatch('roles/fetchAll').then(() => {
+            this.rolesList = this.roles.all
+        })
+        this.$store.dispatch('dre/fetchAll', { withAgencies: true }).then(() => {
+            this.dresList = this.dres.all
+        })
+        this.$store.dispatch('users/fetch', this.$route.params.user).then(() => {
+            this.form.fill(this.user.current)
+            this.form.roles = this.user.current.roles.map(item => item.id)
+            this.form.dres = this.user.current.agencies.map(item => item.id)
+        })
+    },
+    methods: {
+        updateInfos() {
+            this.$swal.confirm_update().then((action) => {
+                if (action.isConfirmed) {
+                    this.form.put('/api/users/info/' + this.user.current.id).then(response => {
+                        if (response.data.status) {
+                            this.$swal.toast_success(response.data.message)
+                            this.$router.push({ name: 'users-index' })
+                        } else {
+                            this.$swal.alert_error(response.data.message)
+                        }
+                    }).catch(error => {
+                        console.log(error)
+                    })
+                }
+            })
+        },
+        updatePassword() {
+            this.$swal.confirm_update().then((action) => {
+                if (action.isConfirmed) {
+                    this.passwordForm.put('/api/users/password/' + this.user.current.id).then(response => {
+                        if (response.data.status) {
+                            this.$swal.toast_success(response.data.message)
+                            this.passwordForm.reset()
+                            this.$router.push({ name: 'users-index' })
+                        } else {
+                            this.$swal.alert_error(response.data.message)
+                        }
+                    }).catch(error => {
+                        console.log(error)
+                    })
+                }
+            })
         }
-      })
     }
-  }
 }
 </script>
