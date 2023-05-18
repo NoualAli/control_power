8e4c970028e9fd71b59d1c489dd8e54c194f7991
nldev <53866581+NoualAli@users.noreply.github.com>
2023-05-18 18:02:02 +0100

    Adding new function to ask user confirmation before leaving the page

--- resources/js/plugins/helpers.js
+++ resources/js/plugins/helpers.js
@@ -8,5 +8,23 @@ export const helpersMixin = {
         currentYear() {
             return new Date().getFullYear()
         },
+    },
+    methods: {
+        /**
+         * Ask user to confirm page leaving
+         *
+         * @param {Closure} next
+         * @param {Object} config
+         */
+        askBeforeLeave(next, config = { icon: 'question', title: '', message: 'Vous n\'avez pas enregistré vos changements, êtes-vous sûr de vouloir quitter la page ?', showConfirmButton: true, showCancelButton: true }) {
+
+            this.$swal.routeLeaveConfirm(config).then(action => {
+                if (action.isConfirmed) {
+                    return next();
+                } else {
+                    return next(false);
+                }
+            })
+        }
     }
 }
--- resources/js/plugins/swal.js
+++ resources/js/plugins/swal.js
@@ -7,18 +7,18 @@ import Swal from 'sweetalert2'
  *
  * @return {any}
  */
-export function confirm ({ icon = 'question', title = '', message = '', showConfirmButton = true, showCancelButton = true }) {
-  return Swal.fire({
-    icon,
-    title,
-    position: 'center',
-    confirmButtonColor: '#00C851',
-    cancelButtonColor: '#CC0000',
-    showConfirmButton,
-    showCancelButton,
-    cancelButtonText: 'Annuler',
-    text: message
-  })
+export function confirm({ icon = 'question', title = '', message = '', showConfirmButton = true, showCancelButton = true }) {
+    return Swal.fire({
+        icon,
+        title,
+        position: 'center',
+        confirmButtonColor: '#00C851',
+        cancelButtonColor: '#CC0000',
+        showConfirmButton,
+        showCancelButton,
+        cancelButtonText: 'Annuler',
+        text: message
+    })
 }
 
 /**
@@ -28,8 +28,19 @@ export function confirm ({ icon = 'question', title = '', message = '', showConf
  *
  * @return {any}
  */
-export function alert_success (message = '', title = 'Succès') {
-  return confirm({ icon: 'success', title, message, showCancelButton: false })
+export function routeLeaveConfirm({ icon = 'question', title = '', message = 'Vous n\'avez pas enregistré vos changements, êtes-vous sûr de vouloir quitter la page ?', showConfirmButton = true, showCancelButton = true }) {
+    return Swal.fire({
+        icon,
+        title,
+        position: 'center',
+        confirmButtonColor: '#00C851',
+        cancelButtonColor: '#CC0000',
+        showConfirmButton,
+        showCancelButton,
+        cancelButtonText: 'Annuler',
+        confirmButtonText: 'Oui',
+        text: message
+    })
 }
 
 /**
@@ -39,8 +50,8 @@ export function alert_success (message = '', title = 'Succès') {
  *
  * @return {any}
  */
-export function alert_error (message = 'Une erreur est survenue', title = 'Erreur') {
-  return confirm({ icon: 'error', title, message, showCancelButton: false })
+export function alert_success(message = '', title = 'Succès') {
+    return confirm({ icon: 'success', title, message, showCancelButton: false })
 }
 
 /**
@@ -50,8 +61,8 @@ export function alert_error (message = 'Une erreur est survenue', title = 'Erreu
  *
  * @return {any}
  */
-export function confirm_destroy (message = 'Voulez-vous vraiment supprimer cette ressource ?') {
-  return confirm({ icon: 'question', title: 'Confirmation', message })
+export function alert_error(message = 'Une erreur est survenue', title = 'Erreur') {
+    return confirm({ icon: 'error', title, message, showCancelButton: false })
 }
 
 /**
@@ -61,8 +72,8 @@ export function confirm_destroy (message = 'Voulez-vous vraiment supprimer cette
  *
  * @return {any}
  */
-export function confirm_update (message = 'Voulez-vous enregistrer vos modifications ?') {
-  return confirm({ icon: 'question', title: 'Confirmation', message })
+export function confirm_destroy(message = 'Voulez-vous vraiment supprimer cette ressource ?') {
+    return confirm({ icon: 'question', title: 'Confirmation', message })
 }
 
 /**
@@ -72,21 +83,8 @@ export function confirm_update (message = 'Voulez-vous enregistrer vos modificat
  *
  * @return {any}
  */
-export function toast ({ icon = 'question', title = '', message = '' }) {
-  Swal.fire({
-    toast: true,
-    icon,
-    title,
-    position: 'bottom-right',
-    timer: 5000,
-    timerProgressBar: true,
-    showConfirmButton: false,
-    text: message,
-    didOpen: (toast) => {
-      toast.addEventListener('mouseenter', Swal.stopTimer)
-      toast.addEventListener('mouseleave', Swal.resumeTimer)
-    }
-  })
+export function confirm_update(message = 'Voulez-vous enregistrer vos modifications ?') {
+    return confirm({ icon: 'question', title: 'Confirmation', message })
 }
 
 /**
@@ -96,8 +94,21 @@ export function toast ({ icon = 'question', title = '', message = '' }) {
  *
  * @return {any}
  */
-export function toast_success (message = '') {
-  return toast({ icon: 'success', title: 'Succès', message })
+export function toast({ icon = 'question', title = '', message = '' }) {
+    Swal.fire({
+        toast: true,
+        icon,
+        title,
+        position: 'bottom-right',
+        timer: 5000,
+        timerProgressBar: true,
+        showConfirmButton: false,
+        text: message,
+        didOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+    })
 }
 
 /**
@@ -107,37 +118,48 @@ export function toast_success (message = '') {
  *
  * @return {any}
  */
-export function toast_error (message = 'Une erreur est survenue') {
-  return toast({ icon: 'error', title: 'Erreur', message })
+export function toast_success(message = '') {
+    return toast({ icon: 'success', title: 'Succès', message })
 }
 
-export function alert_status (error) {
-  const code = error.response.status
-  const statusText = error.response.statusText
-  const title = code + ' ' + statusText
-  const message = code !== 422 ? error.response.data.message : 'Les données fournies sont invalides.'
-  confirm({ icon: 'error', title, message, showCancelButton: false })
+/**
+ * @param string icon
+ * @param string title
+ * @param string message
+ *
+ * @return {any}
+ */
+export function toast_error(message = 'Une erreur est survenue') {
+    return toast({ icon: 'error', title: 'Erreur', message })
 }
 
-export function loading (progress, message = 'Chargement en cours...') {
-  Swal.fire({
-    // title: 'Auto close alert!',
-    html: message + ' <b></b>',
-    // timer: 2000,
-    // timerProgressBar: true,
-    didOpen: () => {
-      Swal.showLoading()
-      const b = Swal.getHtmlContainer().querySelector('b')
-      setInterval(() => {
-        b.textContent = progress + ' %'
-      }, 100)
-    }
-  }).then((result) => {
-    /* Read more about handling dismissals below */
-    if (result.dismiss === Swal.DismissReason.timer) {
-      console.log('I was closed by the timer')
-    }
-  })
+export function alert_status(error) {
+    const code = error.response.status
+    const statusText = error.response.statusText
+    const title = code + ' ' + statusText
+    const message = code !== 422 ? error.response.data.message : 'Les données fournies sont invalides.'
+    confirm({ icon: 'error', title, message, showCancelButton: false })
+}
+
+export function loading(progress, message = 'Chargement en cours...') {
+    Swal.fire({
+        // title: 'Auto close alert!',
+        html: message + ' <b></b>',
+        // timer: 2000,
+        // timerProgressBar: true,
+        didOpen: () => {
+            Swal.showLoading()
+            const b = Swal.getHtmlContainer().querySelector('b')
+            setInterval(() => {
+                b.textContent = progress + ' %'
+            }, 100)
+        }
+    }).then((result) => {
+        /* Read more about handling dismissals below */
+        if (result.dismiss === Swal.DismissReason.timer) {
+            console.log('I was closed by the timer')
+        }
+    })
 }
 // const swal = {
 //   confirm,
