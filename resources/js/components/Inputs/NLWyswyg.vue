3bd756684979f11f8474e695253fc6ec63c6cfe9
nldev <53866581+NoualAli@users.noreply.github.com>
2023-07-21 23:57:42 +0100

    Update NLWyswyg.vue
    Add input class

--- resources/js/components/Inputs/NLWyswyg.vue
+++ resources/js/components/Inputs/NLWyswyg.vue
@@ -1,7 +1,7 @@
 <template>
     <InputContainer :id="getId" :form="form" :label="label" :name="name" :label-required="labelRequired" :length="length"
         :current-length="currentLength">
-        <VueEditor :id="getId" v-model="currentValue" :editor-toolbar="editorSettings"
+        <VueEditor :id="getId" v-model="currentValue" :editor-toolbar="editorSettings" class="input"
             :class="[{ 'is-danger': form?.errors.has(name) }]" :name="name" :autocomplete="autocomplete"
             :autofocus="autofocus" :max-length="length" :placeholder="placeholder || label" :value="currentValue"
             :help-text="helpText" v-bind="$attrs" @input="onInput($event)" @ready="quill => { editorQuill = quill }" />
