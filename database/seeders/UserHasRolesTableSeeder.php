ba3512b8d751a94f83c68961484251385e1e2212
nldev <53866581+NoualAli@users.noreply.github.com>
2023-07-29 20:10:26 +0100

    Update UserHasRolesTableSeeder.php

--- database/seeders/UserHasRolesTableSeeder.php
+++ database/seeders/UserHasRolesTableSeeder.php
@@ -21,298 +21,298 @@ class UserHasRolesTableSeeder extends Seeder
         \DB::table('user_has_roles')->insert(array (
             0 => 
             array (
-                'user_id' => '1',
-                'role_id' => '1',
+                'role_id' => 1,
+                'user_id' => 1,
             ),
             1 => 
             array (
-                'user_id' => '2',
-                'role_id' => '3',
+                'role_id' => 3,
+                'user_id' => 2,
             ),
             2 => 
             array (
-                'user_id' => '4',
-                'role_id' => '2',
+                'role_id' => 2,
+                'user_id' => 4,
             ),
             3 => 
             array (
-                'user_id' => '3',
-                'role_id' => '8',
+                'role_id' => 8,
+                'user_id' => 3,
             ),
             4 => 
             array (
-                'user_id' => '40',
-                'role_id' => '5',
+                'role_id' => 5,
+                'user_id' => 40,
             ),
             5 => 
             array (
-                'user_id' => '41',
-                'role_id' => '6',
+                'role_id' => 6,
+                'user_id' => 41,
             ),
             6 => 
             array (
-                'user_id' => '44',
-                'role_id' => '5',
+                'role_id' => 5,
+                'user_id' => 44,
             ),
             7 => 
             array (
-                'user_id' => '45',
-                'role_id' => '6',
+                'role_id' => 6,
+                'user_id' => 45,
             ),
             8 => 
             array (
-                'user_id' => '46',
-                'role_id' => '11',
+                'role_id' => 11,
+                'user_id' => 46,
             ),
             9 => 
             array (
-                'user_id' => '47',
-                'role_id' => '4',
+                'role_id' => 4,
+                'user_id' => 47,
             ),
             10 => 
             array (
-                'user_id' => '48',
-                'role_id' => '13',
+                'role_id' => 13,
+                'user_id' => 48,
             ),
             11 => 
             array (
-                'user_id' => '49',
-                'role_id' => '13',
+                'role_id' => 13,
+                'user_id' => 49,
             ),
             12 => 
             array (
-                'user_id' => '50',
-                'role_id' => '13',
+                'role_id' => 13,
+                'user_id' => 50,
             ),
             13 => 
             array (
-                'user_id' => '52',
-                'role_id' => '5',
+                'role_id' => 5,
+                'user_id' => 52,
             ),
             14 => 
             array (
-                'user_id' => '53',
-                'role_id' => '5',
+                'role_id' => 5,
+                'user_id' => 53,
             ),
             15 => 
             array (
-                'user_id' => '17',
-                'role_id' => '7',
+                'role_id' => 7,
+                'user_id' => 17,
             ),
             16 => 
             array (
-                'user_id' => '54',
-                'role_id' => '5',
+                'role_id' => 5,
+                'user_id' => 54,
             ),
             17 => 
             array (
-                'user_id' => '19',
-                'role_id' => '10',
+                'role_id' => 10,
+                'user_id' => 19,
             ),
             18 => 
             array (
-                'user_id' => '20',
-                'role_id' => '9',
+                'role_id' => 9,
+                'user_id' => 20,
             ),
             19 => 
             array (
-                'user_id' => '23',
-                'role_id' => '7',
+                'role_id' => 7,
+                'user_id' => 23,
             ),
             20 => 
             array (
-                'user_id' => '55',
-                'role_id' => '6',
+                'role_id' => 6,
+                'user_id' => 55,
             ),
             21 => 
             array (
-                'user_id' => '25',
-                'role_id' => '12',
+                'role_id' => 12,
+                'user_id' => 25,
             ),
             22 => 
             array (
-                'user_id' => '26',
-                'role_id' => '4',
+                'role_id' => 4,
+                'user_id' => 26,
             ),
             23 => 
             array (
-                'user_id' => '27',
-                'role_id' => '4',
+                'role_id' => 4,
+                'user_id' => 27,
             ),
             24 => 
             array (
-                'user_id' => '56',
-                'role_id' => '6',
+                'role_id' => 6,
+                'user_id' => 56,
             ),
             25 => 
             array (
-                'user_id' => '57',
-                'role_id' => '6',
+                'role_id' => 6,
+                'user_id' => 57,
             ),
             26 => 
             array (
-                'user_id' => '58',
-                'role_id' => '11',
+                'role_id' => 11,
+                'user_id' => 58,
             ),
             27 => 
             array (
-                'user_id' => '59',
-                'role_id' => '11',
+                'role_id' => 11,
+                'user_id' => 59,
             ),
             28 => 
             array (
-                'user_id' => '60',
-                'role_id' => '11',
+                'role_id' => 11,
+                'user_id' => 60,
             ),
             29 => 
             array (
-                'user_id' => '61',
-                'role_id' => '11',
+                'role_id' => 11,
+                'user_id' => 61,
             ),
             30 => 
             array (
-                'user_id' => '62',
-                'role_id' => '11',
+                'role_id' => 11,
+                'user_id' => 62,
             ),
             31 => 
             array (
-                'user_id' => '63',
-                'role_id' => '11',
+                'role_id' => 11,
+                'user_id' => 63,
             ),
             32 => 
             array (
-                'user_id' => '64',
-                'role_id' => '11',
+                'role_id' => 11,
+                'user_id' => 64,
             ),
             33 => 
             array (
-                'user_id' => '65',
-                'role_id' => '11',
+                'role_id' => 11,
+                'user_id' => 65,
             ),
             34 => 
             array (
-                'user_id' => '38',
-                'role_id' => '5',
+                'role_id' => 5,
+                'user_id' => 38,
             ),
             35 => 
             array (
-                'user_id' => '39',
-                'role_id' => '6',
+                'role_id' => 6,
+                'user_id' => 39,
             ),
             36 => 
             array (
-                'user_id' => '66',
-                'role_id' => '11',
+                'role_id' => 11,
+                'user_id' => 66,
             ),
             37 => 
             array (
-                'user_id' => '68',
-                'role_id' => '13',
+                'role_id' => 13,
+                'user_id' => 68,
             ),
             38 => 
             array (
-                'user_id' => '69',
-                'role_id' => '11',
+                'role_id' => 11,
+                'user_id' => 69,
             ),
             39 => 
             array (
-                'user_id' => '70',
-                'role_id' => '5',
+                'role_id' => 5,
+                'user_id' => 70,
             ),
             40 => 
             array (
-                'user_id' => '71',
-                'role_id' => '6',
+                'role_id' => 6,
+                'user_id' => 71,
             ),
             41 => 
             array (
-                'user_id' => '72',
-                'role_id' => '13',
+                'role_id' => 13,
+                'user_id' => 72,
             ),
             42 => 
             array (
-                'user_id' => '73',
-                'role_id' => '11',
+                'role_id' => 11,
+                'user_id' => 73,
             ),
             43 => 
             array (
-                'user_id' => '74',
-                'role_id' => '5',
+                'role_id' => 5,
+                'user_id' => 74,
             ),
             44 => 
             array (
-                'user_id' => '75',
-                'role_id' => '6',
+                'role_id' => 6,
+                'user_id' => 75,
             ),
             45 => 
             array (
-                'user_id' => '76',
-                'role_id' => '13',
+                'role_id' => 13,
+                'user_id' => 76,
             ),
             46 => 
             array (
-                'user_id' => '77',
-                'role_id' => '5',
+                'role_id' => 5,
+                'user_id' => 77,
             ),
             47 => 
             array (
-                'user_id' => '78',
-                'role_id' => '6',
+                'role_id' => 6,
+                'user_id' => 78,
             ),
             48 => 
             array (
-                'user_id' => '79',
-                'role_id' => '11',
+                'role_id' => 11,
+                'user_id' => 79,
             ),
             49 => 
             array (
-                'user_id' => '80',
-                'role_id' => '13',
+                'role_id' => 13,
+                'user_id' => 80,
             ),
             50 => 
             array (
-                'user_id' => '81',
-                'role_id' => '5',
+                'role_id' => 5,
+                'user_id' => 81,
             ),
             51 => 
             array (
-                'user_id' => '82',
-                'role_id' => '6',
+                'role_id' => 6,
+                'user_id' => 82,
             ),
             52 => 
             array (
-                'user_id' => '83',
-                'role_id' => '11',
+                'role_id' => 11,
+                'user_id' => 83,
             ),
             53 => 
             array (
-                'user_id' => '84',
-                'role_id' => '13',
+                'role_id' => 13,
+                'user_id' => 84,
             ),
             54 => 
             array (
-                'user_id' => '85',
-                'role_id' => '5',
+                'role_id' => 5,
+                'user_id' => 85,
             ),
             55 => 
             array (
-                'user_id' => '86',
-                'role_id' => '6',
+                'role_id' => 6,
+                'user_id' => 86,
             ),
             56 => 
             array (
-                'user_id' => '87',
-                'role_id' => '11',
+                'role_id' => 11,
+                'user_id' => 87,
             ),
             57 => 
             array (
-                'user_id' => '51',
-                'role_id' => '12',
+                'role_id' => 2,
+                'user_id' => 67,
             ),
             58 => 
             array (
-                'user_id' => '67',
-                'role_id' => '2',
+                'role_id' => 13,
+                'user_id' => 51,
             ),
         ));
         
