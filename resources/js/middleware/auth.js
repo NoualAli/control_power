94b7118cd9ccea516a0ab2e772eadae336d854f6
nldev <53866581+NoualAli@users.noreply.github.com>
2023-08-15 23:06:07 +0100

    Update auth.js

--- resources/js/middleware/auth.js
+++ resources/js/middleware/auth.js
@@ -2,17 +2,17 @@ import store from '~/store'
 import Cookies from 'js-cookie'
 
 export default async (to, from, next) => {
-  const isAuthenticated = store.getters['auth/check']
-  const user = JSON.parse(localStorage.getItem('user'))
-  if (!isAuthenticated && to.path !== '/login') {
-    Cookies.set('intended_url', to.path)
-    Cookies.set('intended_url', to)
-    next({ name: 'login' })
-  } else {
-    if (user?.must_change_password) {
-      next({ name: 'password.new' })
+    const isAuthenticated = store.getters[ 'auth/check' ]
+    const user = JSON.parse(localStorage.getItem('user'))
+    if (!isAuthenticated && to.path !== '/login') {
+        Cookies.set('intended_url', to.path)
+        Cookies.set('intended_url', to)
+        next({ name: 'login' })
     } else {
-      next()
+        if (user?.must_change_password) {
+            next({ name: 'password.new' })
+        } else {
+            next()
+        }
     }
-  }
 }
