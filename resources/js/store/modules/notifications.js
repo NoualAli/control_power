3be1840ae18455238833253969c6856c05599e84
nldev <53866581+NoualAli@users.noreply.github.com>
2023-08-17 13:39:16 +0100

    Update notifications.js

--- resources/js/store/modules/notifications.js
+++ resources/js/store/modules/notifications.js
@@ -12,8 +12,9 @@ export const mutations = {
     SET_TOTAL_UNREAD_MAJOR_FACTS(state, total_unread_major_facts) {
         state.total_unread_major_facts = total_unread_major_facts
     },
-    SET_UNREAD_NOTIFICATIONS(state, totalUnread) {
+    SET_TOTAL_UNREAD_NOTIFICATIONS(state, totalUnread) {
         state.totalUnread = totalUnread
+        // console.log('store updated', totalUnread);
     },
     SET_ALL(state, data) {
         state.all = data
@@ -36,10 +37,15 @@ export const actions = {
         const { data } = await api.get('notifications/total-unread-major-facts')
         commit('SET_TOTAL_UNREAD_MAJOR_FACTS', { total_unread_major_facts: data })
     },
-    async fetchUnreadNotifications({ commit }) {
+    async fetchTotalUnreadNotifications({ commit }) {
         const { data } = await api.get('notifications?withCount')
-        commit('SET_UNREAD_NOTIFICATIONS', { totalUnread: data })
+        commit('SET_TOTAL_UNREAD_NOTIFICATIONS', { totalUnread: data })
     },
+
+    incrementTotal({ commit }) {
+        commit('SET_TOTAL_UNREAD_NOTIFICATIONS', state.totalUnread + 1)
+    },
+
     async readMajorFacts({ commit }) {
         const { data } = await api.post('notifications/read-major-facts')
         commit('SET_TOTAL_UNREAD_MAJOR_FACTS', { total_unread_major_facts: 0 })
