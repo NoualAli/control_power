80aca79daba9e270fd9710802d46d7a46f49478a
nldev <53866581+NoualAli@users.noreply.github.com>
2023-08-17 17:26:09 +0100

    Update ControlCampaignProcessesTableSeeder.php

--- database/seeders/ControlCampaignProcessesTableSeeder.php
+++ database/seeders/ControlCampaignProcessesTableSeeder.php
@@ -14,43 +14,41 @@ class ControlCampaignProcessesTableSeeder extends Seeder
      */
     public function run()
     {
-        
+
 
         \DB::table('control_campaign_processes')->delete();
-        
-        \DB::table('control_campaign_processes')->insert(array (
-            0 => 
-            array (
-                'id' => 1,
+
+        \DB::table('control_campaign_processes')->insert(array(
+            0 =>
+            array(
+
                 'control_campaign_id' => 1,
                 'process_id' => 57,
             ),
-            1 => 
-            array (
-                'id' => 2,
+            1 =>
+            array(
+
                 'control_campaign_id' => 1,
                 'process_id' => 58,
             ),
-            2 => 
-            array (
-                'id' => 3,
+            2 =>
+            array(
+
                 'control_campaign_id' => 1,
                 'process_id' => 8,
             ),
-            3 => 
-            array (
-                'id' => 4,
+            3 =>
+            array(
+
                 'control_campaign_id' => 1,
                 'process_id' => 9,
             ),
-            4 => 
-            array (
-                'id' => 5,
+            4 =>
+            array(
+
                 'control_campaign_id' => 1,
                 'process_id' => 7,
             ),
         ));
-        
-        
     }
 }
