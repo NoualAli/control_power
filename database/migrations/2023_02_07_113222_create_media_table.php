d751f0b5fcc4272970652cbe900b741f0a9d0606
nldev <53866581+NoualAli@users.noreply.github.com>
2023-08-17 17:25:34 +0100

    SET ID as UUID

--- database/migrations/2023_02_07_113222_create_media_table.php
+++ database/migrations/2023_02_07_113222_create_media_table.php
@@ -14,7 +14,7 @@ class CreateMediaTable extends Migration
     public function up()
     {
         Schema::create('media', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
             $table->string('original_name');
             $table->string('hash_name');
             $table->string('folder');
