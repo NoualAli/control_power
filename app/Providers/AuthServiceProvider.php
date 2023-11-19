a83ca30f7e45dce70a5852a88ef95cee8f6d87df
nldev <noualdev@gamil.com>
2023-11-19 21:29:08 +0100

    Install new log-viewer from opcodes

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -26,7 +26,7 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         Gate::before(function ($user, $ability) {
-            return $user->hasRole('root') ? true : null;
+            return strtolower($user->role->code) == 'root' ?: null;
         });
     }
 }
