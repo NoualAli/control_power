8bf18264b75a38070fbb318d3e6aea00fa71468b
nldev <noualdev@gamil.com>
2023-12-06 08:46:44 +0100

    Update Disable this command for production and preproduction Add additional messages to inform user about state

--- app/Console/Commands/SwitchUsersToDevMode.php
+++ app/Console/Commands/SwitchUsersToDevMode.php
@@ -29,9 +29,21 @@ class SwitchUsersToDevMode extends Command
      */
     public function handle()
     {
-        DB::table('users')->update([
-            'password' => Hash::make('123456')
-        ]);
-        return Command::SUCCESS;
+        if (env('APP_ENV') !== 'production' && env('APP_ENV') !== 'production-test') {
+            try {
+                $total = DB::table('users')->update([
+                    'password' => Hash::make('123456')
+                ]);
+                if ($total) {
+                    $total = $total > 1 ? $total . ' lignes ont étaient mise à jour' : $total . ' ligne mise à jour a été mise à jour';
+                    return $this->info('Commande éxecuter avec succès ' . $total);
+                }
+                return $this->info('Aucune ligne n\'a été mise à jour');
+            } catch (\Throwable $th) {
+                $this->error($th->getMessage());
+            }
+        } else {
+            return $this->error('Cette commande n\'est pas autorisée dans un environement de production ou de pre-production');
+        }
     }
 }
