597562096ae8a8633c3b0742cc3874c83fb16723
nldev <noualdev@gamil.com>
2023-12-06 08:20:31 +0100

    Update Correction of permissions names

--- resources/js/components/Sidebar/NLSidebar.vue
+++ resources/js/components/Sidebar/NLSidebar.vue
@@ -7,33 +7,35 @@
         <div class="sidebar-body">
             <div class="sidebar-title" />
             <NLSidebarItem label="Tableau de bord" route="home" iconName="la-tachometer-alt" />
-            <NLSidebarItem label="Utilisateurs" route="users-index" iconName="la-users" v-if="can('view_page_users')" />
-            <NLSidebarItem label="Roles" route="roles-index" iconName="la-user-shield" v-if="can('view_page_roles')" />
-            <NLSidebarItem label="Modules" route="modules-index" iconName="la-user-cog" v-if="can('view_page_modules')" />
-            <NLSidebarItem label="Dre" route="dre-index" iconName="la-building" v-if="can('view_page_dres')" />
-            <NLSidebarItem label="Agences" route="agencies-index" iconName="la-landmark" v-if="can('view_page_agencies')" />
-            <NLSidebarItem label="Catégories" route="categories-index" iconName="la-landmark"
-                v-if="can('view_page_categories')" />
-            <NLSidebarItem label="Familles" route="families-index" iconName="la-tag" v-if="can('view_page_families')" />
-            <NLSidebarItem label="Domaines" route="domains-index" iconName="la-tags" v-if="can('view_page_domains')" />
+            <NLSidebarItem label="Utilisateurs" route="users-index" iconName="la-users" v-if="can('view_user')" />
+            <NLSidebarItem label="Roles" route="roles-index" iconName="la-user-shield" v-if="can('view_role')" />
+            <NLSidebarItem label="Modules" route="modules-index" iconName="la-user-cog" v-if="can('view_module')" />
+            <NLSidebarItem label="Localisations" route="locations-index" iconName="la-building"
+                v-if="can('view_location')" />
+            <!-- <NLSidebarItem label="Dre" route="dre-index" iconName="la-building" v-if="can('view_dres')" />
+            <NLSidebarItem label="Agences" route="agencies-index" iconName="la-landmark" v-if="can('view_agencies')" /> -->
+            <!-- <NLSidebarItem label="Catégories" route="categories-index" iconName="la-landmark"
+                v-if="can('view_categories')" /> -->
+            <NLSidebarItem label="Familles" route="families-index" iconName="la-tag" v-if="can('view_family')" />
+            <NLSidebarItem label="Domaines" route="domains-index" iconName="la-tags" v-if="can('view_domain')" />
             <NLSidebarItem label="Processus" route="processes-index" iconName="la-project-diagram"
-                v-if="can('view_page_processes')" />
+                v-if="can('view_process')" />
             <NLSidebarItem label="Points de contrôle" route="control-points-index" iconName="la-list-alt"
-                v-if="can('view_page_control_points')" />
+                v-if="can('view_control_point')" />
             <NLSidebarItem label="Champs" route="fields-index" iconPrefix="lab" iconName="la-wpforms"
-                v-if="can('view_fields')" />
+                v-if="can('view_field')" />
             <NLSidebarItem label="Répartition des missions de contrôle" route="missions-create" iconName="la-calendar-day"
                 v-if="can('create_mission')" />
             <NLSidebarItem label="Ajouter une campagne de contrôle" route="campaigns-create" iconName="la-calendar-plus"
                 v-if="can('create_control_campaign')" />
             <NLSidebarItem label="Suivi du planning annuel" route="campaigns" iconName="la-calendar"
-                v-if="can('view_page_control_campaigns')" />
+                v-if="can('view_control_campaign')" />
             <NLSidebarItem label="Suivi des réalisations des missions" route="missions" iconName="la-eye"
-                v-if="can('view_page_missions')" />
+                v-if="can('view_mission')" />
             <NLSidebarItem label="Anomalie • Notation • Plan de redressement" route="global-details" iconPrefix="lab"
-                iconName="la-stack-overflow" v-if="can('view_page_mission_details')" />
+                iconName="la-stack-overflow" v-if="can('view_mission_details')" />
             <NLSidebarItem label="Faits majeur" route="major-facts" iconName="la-exclamation-triangle"
-                v-if="can('view_page_major_facts')" />
+                v-if="can('view_major_fact')" />
             <NLSidebarItem label="Backup DB" route="admin-backup-db" v-if="is('root')" iconName="la-database" />
             <NLSidebarItem label="Fichiers" route="admin-server-files" v-if="is('root')" iconName="la-file-alt" />
             <a class="sidebar-item" href="/log-viewer" target="_blank" v-if="is('root')">
@@ -45,7 +47,7 @@
             <!-- <NLSidebarItem label="Bugs" route="bugs-index" iconName="la-bug" /> -->
             <!-- <NLSidebarItem label="Validation des rapports" route="missions-not-validated" iconName="la-clipboard-check"
                 v-if="can('validate_report')" /> -->
-            <!-- <router-link v-if="can('view_page_major_facts,view_major_fact')" class="sidebar-item"
+            <!-- <router-link v-if="can('view_major_facts,view_major_fact')" class="sidebar-item"
                 :class="{ 'has-badge': unreadMajorFacts }" :to="{ name: 'major-facts' }">
                 <i class="las la-exclamation-triangle sidebar-icon" />
                 <span class="icon-text">
