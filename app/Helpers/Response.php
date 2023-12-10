af160f5741533fc6e5e4bf58097d0d241affbc01
nldev <noualdev@gamil.com>
2023-12-10 09:45:09 +0100

    Update Change default error message and add response code as param

--- app/Helpers/Response.php
+++ app/Helpers/Response.php
@@ -3,18 +3,18 @@ if (!function_exists('throwedError')) {
     function throwedError(\Throwable $th)
     {
         return response()->json([
-            'message' =>  $th->getMessage(),
+            'message' => in_array(env('APP_ENV'), ['production', 'production-test']) ? DEFAULT_ERROR_MESSAGE : $th->getMessage(),
             'status' => false
         ], SERVER_ERROR);
     }
 }
 
 if (!function_exists('actionResponse')) {
-    function actionResponse(bool $status, string $successMessage, ?string $errorMessage = CREATE_ERROR)
+    function actionResponse(bool $status, string $successMessage, ?string $errorMessage = DEFAULT_ERROR_MESSAGE, int $code = SERVER_SUCCESS)
     {
         return response()->json([
             'message' => $status ? $successMessage : $errorMessage,
             'status' => $status
-        ], SERVER_SUCCESS);
+        ], $code);
     }
 }
