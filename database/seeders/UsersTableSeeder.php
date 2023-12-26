2996c2945efa625babde535258e49968eca3ecc2
nldev <noualdev@gamil.com>
2023-12-26 14:41:30 +0100

    Update CDCR username

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -20,50 +20,66 @@ class UsersTableSeeder extends Seeder
     {
         DB::transaction(function () {
             try {
-                $roles = DB::table('roles')->whereIn('code', ['da', 'cdc', 'dre'])->get();
+                $roles = DB::table('roles')->whereIn('code', ['da', 'cdc', 'dre', 'cdcr'])->get();
                 $updated = 0;
+                $currentUser = collect([]);
                 foreach ($roles as $role) {
                     $currentRole = $role;
                     $users = collect([]);
                     if ($role->code == 'da') {
                         $users = DB::table('users as u')
-                            ->select([DB::raw("CONCAT('DA-',a.code) as new_username"), 'username', 'u.id'])
+                            ->select([DB::raw("CONCAT('DA-',a.code) as new_username"), 'username', 'u.id', 'r.code'])
                             ->join('user_has_agencies as uha', 'u.id', 'uha.user_id')
                             ->join('agencies as a', 'a.id', 'uha.agency_id')
-                            ->where('active_role_id', $role->id)
-                            ->groupBy(['a.code', 'username', 'u.id'])
+                            ->join('roles as r', 'r.id', 'u.active_role_id')
+                            ->where('active_role_id', intVal($role->id))
+                            ->groupBy(['a.code', 'username', 'u.id', 'r.code'])
                             ->get();
                     } elseif ($role->code == 'cdc') {
                         $users = DB::table('users as u')
-                            ->select([DB::raw("CONCAT('CDC-',d.code) as new_username"), 'username', 'u.id'])
+                            ->select([DB::raw("CONCAT('CDC-',d.code) as new_username"), 'username', 'u.id', 'r.code'])
                             ->join('user_has_agencies as uha', 'u.id', 'uha.user_id')
                             ->join('agencies as a', 'a.id', 'uha.agency_id')
                             ->join('dres as d', 'd.id', 'a.dre_id')
-                            ->where('active_role_id', $role->id)
-                            ->groupBy(['d.code', 'username', 'u.id'])
+                            ->join('roles as r', 'r.id', 'u.active_role_id')
+                            ->where('active_role_id', intVal($role->id))
+                            ->groupBy(['d.code', 'username', 'u.id', 'r.code'])
                             ->get();
                     } elseif ($role->code == 'dre') {
                         $users = DB::table('users as u')
-                            ->select([DB::raw("CONCAT('DRE-',d.code) as new_username"), 'username', 'u.id'])
+                            ->select([DB::raw("CONCAT('DRE-',d.code) as new_username"), 'username', 'u.id', 'r.code'])
                             ->join('user_has_agencies as uha', 'u.id', 'uha.user_id')
                             ->join('agencies as a', 'a.id', 'uha.agency_id')
                             ->join('dres as d', 'd.id', 'a.dre_id')
-                            ->where('active_role_id', $role->id)
-                            ->groupBy(['d.code', 'username', 'u.id'])
+                            ->join('roles as r', 'r.id', 'u.active_role_id')
+                            ->where('active_role_id', intVal($role->id))
+                            ->groupBy(['d.code', 'username', 'u.id', 'r.code'])
+                            ->get();
+                    } elseif ($role->code == 'cdcr') {
+                        $users = DB::table('users as u')
+                            ->select(['username', 'u.id', 'r.code'])
+                            ->join('roles as r', 'r.id', 'u.active_role_id')
+                            ->where('active_role_id', intVal($role->id))
+                            ->groupBy(['username', 'u.id', 'r.code'])
                             ->get();
                     }
                     foreach ($users as $user) {
+                        if ($user?->code == 'cdcr') {
+                            $user->new_username = 'CDCR';
+                        }
                         $currentUser = $user;
                         $result = DB::table('users')->where('id', $user->id)->update([
                             'username' => $user->new_username
                         ]);
-                        Notification::send(User::findOrFail($user->id), new UpdateUsernamesNotification($role, $user));
+                        if (env('APP_ENV') !== 'dev') {
+                            Notification::send(User::findOrFail($user->id), new UpdateUsernamesNotification($role, $user));
+                        }
                         if ($result) {
                             $updated += 1;
                         }
                     }
                 }
-                print_r('Total utilisateurs mis à jour: ' . $updated . '\n');
+                print_r('Total utilisateurs mis à jour: ' . $updated . '\\n');
             } catch (\Throwable $th) {
                 dd($th->getMessage(), $currentRole, $currentUser);
             }
