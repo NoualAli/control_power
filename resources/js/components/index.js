3dd88b50847307e52cdecd7f5d139e277b07699d
nldev <noualdev@gamil.com>
2023-12-26 20:33:52 +0100

    Add NLInputDate

--- resources/js/components/index.js
+++ resources/js/components/index.js
@@ -2,6 +2,7 @@ import Child from './Child.vue'
 import Alert from './Alert.vue'
 import InputContainer from './Inputs/InputContainer.vue'
 import NLInput from './Inputs/NLInput.vue'
+import NLInputDate from './Inputs/NLInputDate.vue'
 import NLTagInput from './Inputs/NLTagInput.vue'
 import NLTextarea from './Inputs/NLTextarea.vue'
 import NLButton from './Inputs/NLButton.vue'
@@ -33,6 +34,7 @@ export function useComponents(app) {
     [
         // NLPageLoader,
         // NLComponentLoader
+        NLInputDate,
         NLForm,
         NLRadios,
         NLCheckboxes,
