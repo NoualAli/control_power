5dd60884a28874a4fda2e361eac688a1a34d51f1
nldev <noualdev@gamil.com>
2024-01-03 09:05:31 +0100

    Remove Intervention/Image update project dependencies

--- config/app.php
+++ config/app.php
@@ -165,7 +165,8 @@ return [
         /*
          * Package Service Providers...
          */
-        Intervention\Image\ImageServiceProvider::class,
+        SqlsrvAvoidError\Providers\SqlsrvAvoidErrorProvider::class,
+
         /*
          * Application Service Providers...
          */
@@ -226,7 +227,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Image' => Intervention\Image\Facades\Image::class,
         'Agent' => Jenssegers\Agent\Facades\Agent::class,
     ],
 
