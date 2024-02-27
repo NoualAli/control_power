257cd1cf51d5113558f987fdadf13ca2eb66176e
nldev <noualdev@gamil.com>
2024-02-27 18:24:06 +0100

    Setup users to create

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
         // $this->call(NotificationGroupSeeder::class);
         // $this->call(NotificationTypeSeeder::class);
         $this->call(UserHasNotificationsSeeder::class);
--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -21,67 +21,156 @@ class UsersTableSeeder extends Seeder
     {
         DB::transaction(function () {
             try {
-                print_r("  Mise à jour des utilisateurs en cours \n");
-                $roles = DB::table('roles')->whereIn('code', ['da', 'cdc', 'dre', 'cdcr'])->get();
-                $updated = 0;
-                // $updated += DB::table('users')->where('username', 'DGA')->update(['active_role_id' => 15]);
-                $currentUser = collect([]);
-                foreach ($roles as $role) {
-                    $currentRole = $role;
-                    $users = collect([]);
-                    if ($role->code == 'da') {
-                        $users = DB::table('users as u')
-                            ->select([DB::raw("CONCAT('DA-',a.code) as new_username"), 'username', 'u.id', 'r.code'])
-                            ->join('user_has_agencies as uha', 'u.id', 'uha.user_id')
-                            ->join('agencies as a', 'a.id', 'uha.agency_id')
-                            ->join('roles as r', 'r.id', 'u.active_role_id')
-                            ->where('active_role_id', intVal($role->id))
-                            ->groupBy(['a.code', 'username', 'u.id', 'r.code'])
-                            ->get();
-                    } elseif ($role->code == 'cdc') {
-                        $users = DB::table('users as u')
-                            ->select([DB::raw("CONCAT('CDC-',d.code) as new_username"), 'username', 'u.id', 'r.code'])
-                            ->join('user_has_agencies as uha', 'u.id', 'uha.user_id')
-                            ->join('agencies as a', 'a.id', 'uha.agency_id')
-                            ->join('dres as d', 'd.id', 'a.dre_id')
-                            ->join('roles as r', 'r.id', 'u.active_role_id')
-                            ->where('active_role_id', intVal($role->id))
-                            ->groupBy(['d.code', 'username', 'u.id', 'r.code'])
-                            ->get();
-                    } elseif ($role->code == 'dre') {
-                        $users = DB::table('users as u')
-                            ->select([DB::raw("CONCAT('DRE-',d.code) as new_username"), 'username', 'u.id', 'r.code'])
-                            ->join('user_has_agencies as uha', 'u.id', 'uha.user_id')
-                            ->join('agencies as a', 'a.id', 'uha.agency_id')
-                            ->join('dres as d', 'd.id', 'a.dre_id')
-                            ->join('roles as r', 'r.id', 'u.active_role_id')
-                            ->where('active_role_id', intVal($role->id))
-                            ->groupBy(['d.code', 'username', 'u.id', 'r.code'])
-                            ->get();
-                    } elseif ($role->code == 'cdcr') {
-                        $users = DB::table('users as u')
-                            ->select(['username', 'u.id', 'r.code'])
-                            ->join('roles as r', 'r.id', 'u.active_role_id')
-                            ->where('active_role_id', intVal($role->id))
-                            ->groupBy(['username', 'u.id', 'r.code'])
-                            ->get();
-                    }
-                    foreach ($users as $user) {
-                        if ($user?->code == 'cdcr') {
-                            $user->new_username = 'CDCR';
-                        }
-                        $currentUser = $user;
-                        $result = DB::table('users')->where('id', $user->id)->update([
-                            'username' => $user->new_username
-                        ]);
-                        if (env('APP_ENV') !== 'dev') {
-                        }
-                        Notification::send(User::findOrFail($user->id), new UpdateUsernamesNotification($role, $user));
-                        $updated += 1;
-                    }
+                print_r("  Création des utilisateurs en cours \n");
+                $inserted = DB::table('users')->insert([
+                    [
+                        'last_name' => 'OUKID',
+                        'first_name' => 'OUARDIA',
+                        'username' => 'O.OUKID',
+                        'registration_number' => '10347',
+                        'email' => 'O.OUKIDOuardia@bna.dz',
+                        'phone' => '0795451425',
+                        'gender' => 2,
+                        'active_role_id' => 17,
+                        'password' => Hash::make('Azerty123'),
+                        'is_active' => 1,
+                        'created_at' => now(),
+                        'must_change_password' => 1,
+                    ],
+                    [
+                        'last_name' => 'HADJOUMEUR',
+                        'first_name' => 'REDA',
+                        'username' => 'R.HADJOUMEUR',
+                        'email' => 'R.HADJOUMEUR@bna.dz',
+                        'registration_number' => '11974',
+                        'phone' => NULL,
+                        'gender' => 1,
+                        'active_role_id' => 17,
+                        'password' => Hash::make('Azerty123'),
+                        'is_active' => 1,
+                        'created_at' => now(),
+                        'must_change_password' => 1,
+                    ],
+                    [
+                        'last_name' => 'ASSOUANE',
+                        'first_name' => 'ASSIA',
+                        'username' => 'A.ASSOUANE',
+                        'registration_number' => '10527',
+                        'email' => 'A.MAKHLOUF@bna.dz',
+                        'phone' => NULL,
+                        'gender' => 2,
+                        'active_role_id' => 17,
+                        'password' => Hash::make('Azerty123'),
+                        'is_active' => 1,
+                        'created_at' => now(),
+                        'must_change_password' => 1,
+                    ],
+                    [
+                        'last_name' => 'SEDDIKI',
+                        'first_name' => 'KHEIRA',
+                        'username' => 'K.SEDDIKI',
+                        'registration_number' => '3757',
+                        'email' => 'K.SEDDIKI@bna.dz',
+                        'phone' => NULL,
+                        'gender' => 2,
+                        'active_role_id' => 17,
+                        'password' => Hash::make('Azerty123'),
+                        'is_active' => 1,
+                        'created_at' => now(),
+                        'must_change_password' => 1,
+                    ],
+                    [
+                        'last_name' => 'MERDJI',
+                        'first_name' => 'ISMA',
+                        'username' => 'I.MERDJI',
+                        'registration_number' => '10305',
+                        'email' => 'I.MERDJI@bna.dz',
+                        'phone' => NULL,
+                        'gender' => 2,
+                        'active_role_id' => 17,
+                        'password' => Hash::make('Azerty123'),
+                        'is_active' => 1,
+                        'created_at' => now(),
+                        'must_change_password' => 1,
+                    ],
+                    [
+                        'last_name' => 'DJERRAD',
+                        'first_name' => 'HOUDA',
+                        'username' => 'H.DJERRAD',
+                        'registration_number' => '10172',
+                        'email' => 'H.DJERAD@bna.dz',
+                        'phone' => NULL,
+                        'gender' => 2,
+                        'active_role_id' => 17,
+                        'password' => Hash::make('Azerty123'),
+                        'is_active' => 1,
+                        'created_at' => now(),
+                        'must_change_password' => 1,
+                    ],
+                    [
+                        'last_name' => 'KHELIOUENE',
+                        'first_name' => 'REDOUANE',
+                        'username' => 'R.KHELIOUENE',
+                        'registration_number' => '10997',
+                        'email' => 'R.KHELIOUEN@bna.dz',
+                        'phone' => NULL,
+                        'gender' => 1,
+                        'active_role_id' => 17,
+                        'password' => Hash::make('Azerty123'),
+                        'is_active' => 1,
+                        'created_at' => now(),
+                        'must_change_password' => 1,
+                    ],
+                    [
+                        'last_name' => 'LATRECHE',
+                        'first_name' => 'SAAD',
+                        'username' => 'S.LATRECHE',
+                        'registration_number' => '11388',
+                        'email' => 'S.LATRECHE@bna.dz',
+                        'phone' => NULL,
+                        'gender' => 1,
+                        'active_role_id' => 17,
+                        'password' => Hash::make('Azerty123'),
+                        'is_active' => 1,
+                        'created_at' => now(),
+                        'must_change_password' => 1,
+                    ],
+                    [
+                        'last_name' => 'DEBBAGHI',
+                        'first_name' => 'SALIMA',
+                        'username' => 'S.DEBBAGHI',
+                        'registration_number' => '14854',
+                        'email' => 'S.DEBBAGHI@bna.dz',
+                        'phone' => NULL,
+                        'gender' => 2,
+                        'active_role_id' => 17,
+                        'password' => Hash::make('Azerty123'),
+                        'is_active' => 1,
+                        'created_at' => now(),
+                        'must_change_password' => 1,
+                    ],
+                    [
+                        'last_name' => 'CHIKHI',
+                        'first_name' => 'HANANE',
+                        'username' => 'H.CHIKHI',
+                        'registration_number' => '14850',
+                        'email' => 'H.CHIKHI@bna.dz',
+                        'phone' => NULL,
+                        'gender' => 1,
+                        'active_role_id' => 17,
+                        'password' => Hash::make('Azerty123'),
+                        'is_active' => 1,
+                        'created_at' => now(),
+                        'must_change_password' => 1,
+                    ],
+                ]);
+                if ($inserted) {
+                    print_r("  Création des utilisateurs terminer avec succès ! \n");
+                } else {
+                    print_r("  Création des utilisateurs terminer avec des erreurs \n");
                 }
             } catch (\Throwable $th) {
-                dd($th->getMessage(), $currentRole, $currentUser);
+                dd($th->getMessage());
             }
         });
     }
