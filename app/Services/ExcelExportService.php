ae6231a8baa260d7fdd4c5553808cf7877ba66aa
NLDev <noualdev@gmail.com>
2024-03-10 18:04:34 +0100

    Update - Keep only pagination choice

--- app/Services/ExcelExportService.php
+++ app/Services/ExcelExportService.php
@@ -33,33 +33,7 @@ class ExcelExportService
 
         if (count($config)) {
             extract($config);
-            $keep_filters = boolVal(intVal($keep_filters));
-            $keep_sorting = boolVal(intVal($keep_sorting));
-            $keep_search = boolVal(intVal($keep_search));
-            $keep_current_page = boolVal(intVal($keep_current_page));
             $keep_current_pagination = boolVal(intVal($keep_current_pagination));
-            $search_columns = isset($search_columns) ? $search_columns : [];
-
-            if ($keep_filters) {
-                $filters = request('filter');
-                $model = $model->filter($filters);
-            }
-
-            if ($keep_sorting) {
-                $sorting = request('sort');
-                if ($sorting) {
-                    $model = $model->sortByMultiple($sorting);
-                }
-            } else {
-                $model = $model->reorder();
-            }
-
-            if ($keep_search) {
-                $search = request('search');
-                if ($search) {
-                    $model = $model->search($search_columns, $search);
-                }
-            }
 
             if ($keep_current_pagination) {
                 $perPage = request('perPage', 10);
--- resources/js/Modals/ExcelExportModal.vue
+++ resources/js/Modals/ExcelExportModal.vue
@@ -3,17 +3,18 @@
         <template #title>
             Configurer votre exportation excel
         </template>
+
         <template #default>
             <NLGrid class="box">
-                <NLColumn lg="6">
+                <!-- <NLColumn lg="6">
                     <NLSwitch label="Garder la recherche actuelle" v-model="keep_search" />
-                </NLColumn>
+                </NLColumn> -->
                 <!-- <NLColumn lg="6">
                     <NLSwitch label="Garder les filtres actuels" v-model="keep_filters" />
                 </NLColumn> -->
-                <NLColumn lg="6">
+                <!-- <NLColumn lg="6">
                     <NLSwitch label="Garder le trie actuel" v-model="keep_sorting" />
-                </NLColumn>
+                </NLColumn> -->
                 <NLColumn lg="6">
                     <NLSwitch label="Garder la pagination actuelle" v-model="keep_current_pagination" />
                 </NLColumn>
@@ -22,6 +23,7 @@
                 </NLColumn> -->
             </NLGrid>
         </template>
+
         <template #footer>
             <button class="btn btn-office-excel has-icon" @click.stop="exportExcel">
                 <NLIcon name="table" />
@@ -54,29 +56,29 @@ export default {
     watch: {
         show(newVal, oldVal) {
             if (!newVal) {
-                this.keep_filters = false
-                this.keep_sorting = false
+                // this.keep_filters = false
+                // this.keep_sorting = false
                 this.keep_current_pagination = false
-                this.keep_current_page = false
-                this.keep_search = false
+                // this.keep_current_page = false
+                // this.keep_search = false
             } else {
                 this.url = this.route
             }
         },
-        keep_filters(newVal, oldVal) {
-            if (newVal !== oldVal && newVal) {
-                this.url += '&export[keep_filters]=1'
-            } else {
-                this.url += '&export[keep_filters]=0'
-            }
-        },
-        keep_sorting(newVal, oldVal) {
-            if (newVal !== oldVal && newVal) {
-                this.url += '&export[keep_sorting]=1'
-            } else {
-                this.url += '&export[keep_sorting]=0'
-            }
-        },
+        // keep_filters(newVal, oldVal) {
+        //     if (newVal !== oldVal && newVal) {
+        //         this.url += '&export[keep_filters]=1'
+        //     } else {
+        //         this.url += '&export[keep_filters]=0'
+        //     }
+        // },
+        // keep_sorting(newVal, oldVal) {
+        //     if (newVal !== oldVal && newVal) {
+        //         this.url += '&export[keep_sorting]=1'
+        //     } else {
+        //         this.url += '&export[keep_sorting]=0'
+        //     }
+        // },
         keep_current_pagination(newVal, oldVal) {
             if (newVal !== oldVal && newVal) {
                 this.url += '&export[keep_current_pagination]=1'
@@ -84,56 +86,56 @@ export default {
                 this.url += '&export[keep_current_pagination]=0'
             }
         },
-        keep_current_page(newVal, oldVal) {
-            if (newVal !== oldVal && newVal) {
-                this.url += '&export[keep_current_page]=1'
-            } else {
-                this.url += '&export[keep_current_page]=0'
-            }
-        },
-        keep_search(newVal, oldVal) {
-            if (newVal !== oldVal && newVal) {
-                this.url += '&export[keep_search]=1'
-            } else {
-                this.url += '&export[keep_search]=0'
-            }
-        },
+        // keep_current_page(newVal, oldVal) {
+        //     if (newVal !== oldVal && newVal) {
+        //         this.url += '&export[keep_current_page]=1'
+        //     } else {
+        //         this.url += '&export[keep_current_page]=0'
+        //     }
+        // },
+        // keep_search(newVal, oldVal) {
+        //     if (newVal !== oldVal && newVal) {
+        //         this.url += '&export[keep_search]=1'
+        //     } else {
+        //         this.url += '&export[keep_search]=0'
+        //     }
+        // },
     },
     data() {
         return {
             url: this.route,
-            keep_filters: false,
-            keep_sorting: false,
+            // keep_filters: false,
+            // keep_sorting: false,
             keep_current_pagination: false,
-            keep_current_page: false,
-            keep_search: false
+            // keep_current_page: false,
+            // keep_search: false
         }
     },
     updated() {
         if (this.hideOptions) {
             this.exportExcel()
         }
-        this.getUrl('keep_filters', this.keep_filters)
-        this.getUrl('keep_sorting', this.keep_sorting)
+        // this.getUrl('keep_filters', this.keep_filters)
+        // this.getUrl('keep_sorting', this.keep_sorting)
         this.getUrl('keep_current_pagination', this.keep_current_pagination)
-        this.getUrl('keep_current_page', this.keep_current_page)
-        this.getUrl('keep_search', this.keep_search)
+        // this.getUrl('keep_current_page', this.keep_current_page)
+        // this.getUrl('keep_search', this.keep_search)
     },
     created() {
-        this.getUrl('keep_filters', this.keep_filters)
-        this.getUrl('keep_sorting', this.keep_sorting)
+        // this.getUrl('keep_filters', this.keep_filters)
+        // this.getUrl('keep_sorting', this.keep_sorting)
         this.getUrl('keep_current_pagination', this.keep_current_pagination)
-        this.getUrl('keep_current_page', this.keep_current_page)
-        this.getUrl('keep_search', this.keep_search)
+        // this.getUrl('keep_current_page', this.keep_current_page)
+        // this.getUrl('keep_search', this.keep_search)
     },
     methods: {
         close() {
             this.url = this.route
-            this.keep_filters = false
-            this.keep_sorting = false
+            // this.keep_filters = false
+            // this.keep_sorting = false
             this.keep_current_pagination = false
-            this.keep_current_page = false
-            this.keep_search = false
+            // this.keep_current_page = false
+            // this.keep_search = false
             this.$emit('close')
         },
         exportExcel() {
@@ -141,31 +143,31 @@ export default {
             this.close()
         },
         getUrl(filter, value) {
-            if (filter == 'keep_filters' && value) {
-                this.url += '&export[keep_filters]=1'
-            } else if (filter == 'keep_filters' && !value) {
-                this.url += '&export[keep_filters]=0'
-            }
-            if (filter == 'keep_sorting' && value) {
-                this.url += '&export[keep_sorting]=1'
-            } else if (filter == 'keep_sorting' && !value) {
-                this.url += '&export[keep_sorting]=0'
-            }
+            // if (filter == 'keep_filters' && value) {
+            //     this.url += '&export[keep_filters]=1'
+            // } else if (filter == 'keep_filters' && !value) {
+            //     this.url += '&export[keep_filters]=0'
+            // }
+            // if (filter == 'keep_sorting' && value) {
+            //     this.url += '&export[keep_sorting]=1'
+            // } else if (filter == 'keep_sorting' && !value) {
+            //     this.url += '&export[keep_sorting]=0'
+            // }
             if (filter == 'keep_current_pagination' && value) {
                 this.url += '&export[keep_current_pagination]=1'
             } else if (filter == 'keep_current_pagination' && !value) {
                 this.url += '&export[keep_current_pagination]=0'
             }
-            if (filter == 'keep_current_page' && value) {
-                this.url += '&export[keep_current_page]=1'
-            } else if (filter == 'keep_current_page' && !value) {
-                this.url += '&export[keep_current_page]=0'
-            }
-            if (filter == 'keep_search' && value) {
-                this.url += '&export[keep_search]=1'
-            } else if (filter == 'keep_search' && !value) {
-                this.url += '&export[keep_search]=0'
-            }
+            // if (filter == 'keep_current_page' && value) {
+            //     this.url += '&export[keep_current_page]=1'
+            // } else if (filter == 'keep_current_page' && !value) {
+            //     this.url += '&export[keep_current_page]=0'
+            // }
+            // if (filter == 'keep_search' && value) {
+            //     this.url += '&export[keep_search]=1'
+            // } else if (filter == 'keep_search' && !value) {
+            //     this.url += '&export[keep_search]=0'
+            // }
         }
     }
 }
