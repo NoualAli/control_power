ec37fe48e367b5c7836ac393691eceae9ca950d4
NLDev <noualdev@gmail.com>
2024-03-18 14:25:55 +0100

    Update - Update existing missions state from 8 to 7

--- database/seeders/MissionsTableSeeder.php
+++ database/seeders/MissionsTableSeeder.php
@@ -2,11 +2,8 @@
 
 namespace Database\Seeders;
 
-use App\Models\Mission;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Schema;
 
 class MissionsTableSeeder extends Seeder
 {
@@ -19,7 +16,7 @@ class MissionsTableSeeder extends Seeder
     public function run()
     {
         DB::transaction(function () {
-            DB::table('missions')->where('current_state', 7)->update(['current_state' => 8]);
+            DB::table('missions')->where('current_state', 8)->update(['current_state' => 7]);
         });
     }
 }
